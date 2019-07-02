3248c30f36a1efb86dad453519faf822961f18fd
Romeo Axpuac <misinicialesbray@gmail.com>
2019-07-01 20:35:42 -0600

    Create index.php

---
+++ DockerCompose/Servidor/index.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+<style type= "text/css">
+<!--
+body{background: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjAkOvNwKjwEf0LvCRhM1XKYrtCuK9Ht_UDMUE86MGvLqtr0fl") no-repeat fix$
+        background-attachment: fixed;
+        background-size: cover;
+}
+-->     
+ p { 
+        color: white; font-family: Verdana;
+        margin: 20px auto;
+        width: 350px;
+  }
+  .informacion {
+        background: #D3D3D3 center no-repeat;
+        background-position: 15px 50%;
+        color: black;
+        font-family: Arial;
+        font-size: 25px;
+        text-align: center;
+        padding: 28px 20px 25px 45px;
+        border-top: 5px double #00529B;
+        border-bottom: 5px double #00529B;
+        }
+        body {
+            background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjAkOvNwKjwEf0LvCRhM1XKYrtCuK9Ht_UDMUE86MGvLqtr0fl");
+        }
+</style>
+<title></title>
+
+</head>
+<body>
+<div>
+
+</div>
+        <center>
+
+
+        <img src="http://www.aikidomallorca.es/wp-content/uploads/2017/04/bienvenido.png" width="400" height="341">
+
+
+        <td>
+        <form id="form1" name="form1" action= "http://ec2-13-59-177-105.us-east-2.compute.amazonaws.com:3000/producto" method="POST" ">
+        <p><span class="Estilo16">Nombre Producto<br />
+        <label>
+                <input name="nombre" type="text" id="nombre" />
+        </label>
+        <br />
+        Cantidad<br />
+        <input name="cantidad" type="text" id="cantidad" />
+        <br />
+        Precio<br />
+        <input  name="precio" type="text" id="precio" />
+        </span><br />
+        <label>
+        <input type="submit" name="Submit" value="Cargar Productos" />
+        </label>
+        </p>
+        </form>
+        </td> 
+</center>
+
+</body>
+</html>
